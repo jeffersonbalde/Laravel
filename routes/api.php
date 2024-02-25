b9c2e68dd8c4509356bc5f94e919fdda49cdcf8c
jeffersonbalde <jeffersonbalde8@gmail.com>
2024-02-25 13:36:16 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/name', function () {
+    return response()->json([
+        'name' => [
+                [
+                    'Amiee' => 'Gwapa'
+                ]   
+            ]
+        ]);
+});
