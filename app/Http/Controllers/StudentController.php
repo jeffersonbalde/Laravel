ab4eb5d3f444167ad4fee1f1be4932423b1c375d
jeffersonbalde <jeffersonbalde8@gmail.com>
2024-03-03 14:51:47 +0800

    test

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Students;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index() {
-        return 'Students';
+    public function index() 
+    {
+        $data = Students::all();
+        // return view('students.index', $data);  
+        return view('students.index', ['students' => $data]);
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,7 @@
-<?php
+    <?php
 
 use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/', function() {
-//     return 'shit';
-// });
-
-// Route::match(['get','post'], '/', function() {
-//     return 'test';
-// });
-
-// Route::view('/', 'welcome');
-
-Route::get('/test', function (Request $request) {
-    dd($request);
-});
-
-Route::get('test2', function () {
-    return response('<h1>test2</h1>', 200)
-        ->header('Content-Type', 'text/html')
-        ->header('jeff', 'jefferson');
-});
-
-Route::get('test3/{id}', function ($id) {
-    return response($id . ' ' . ' php', 200);
-});
-
-Route::get('/test4', function () {
-    $path = public_path().'/sample.txt';
-    $name = "test.txt";
-    $headers = array(
-        'Content-type : application/text-plain'
-    );
-
-    return response()->download($path, $name, $headers);
-});
 
 Route::get('/', [StudentController::class, 'index']);
